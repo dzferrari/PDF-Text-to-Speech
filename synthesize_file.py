f62e3f24d0779b8b2f6bf2791023b9427d1abcf5
dzferrari <72628389+dzferrari@users.noreply.github.com>
2023-01-21 02:21:58 -0300

    Generate audio from text

--- synthesize_file.py
+++ synthesize_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 #
+# Generate Audio from text files using Google Cloud TTS API.
+#
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
